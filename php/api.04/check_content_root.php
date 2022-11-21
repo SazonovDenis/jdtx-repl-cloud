facf066883f3693a4c5e8e2603a49be4b5a38e04
Сазонов Денис <d.sazonov@kazinfosystems.kz>
2022-11-21 23:55:35 +0600

    -

--- php/api.04/check_content_root.php
+++ php/api.04/check_content_root.php
@@ -2,7 +2,7 @@
 
 if (!is_dir($content_root)) {
   header("HTTP/1.1 415 Unsupported Media Type");
-  print('{"result": "error", "error": "content_root in not exists: \''.$content_root.'\' (\'.\' at \''.realpath(".").'\''.'"}');
+  print('{"result": "error", "error": "content_root in not exists: \''.$content_root.'\' (at \''.realpath(".").')\''.'"}');
   exit();
 }
 
